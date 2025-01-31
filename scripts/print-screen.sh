d44bbd2f41b8e8e1d7dfcf2da22ab5bf5594697b
axel-paillaud <contact@axelweb.fr>
2025-01-31 13:01:43 +0100

    add script to handle printscreen

---
+++ scripts/print-screen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCREENSHOT_DIR="$HOME/Images/Captures d’écran"
+mkdir -p "$SCREENSHOT_DIR"
+
+FILENAME="screenshot_$(date +'%Y-%m-%d_%H-%M-%S').png"
+FULL_PATH="$SCREENSHOT_DIR/$FILENAME"
+
+grim -g "$(slurp -c '#00000000')" "$FULL_PATH"
+
+notify-send "Capture d'écran" "Enregistrée dans $FULL_PATH"
+
